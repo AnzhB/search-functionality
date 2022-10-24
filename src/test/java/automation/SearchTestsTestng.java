139c92c500a8c99daaba7c938cbd49e5ba41c250
Anzhalika Baradzina <abaradzina@corelogic.com>
2022-10-24 16:26:33 +0200

    updated SearchTestsTestng added

--- src/test/java/automation/SearchTestsTestng.java
+++ src/test/java/automation/SearchTestsTestng.java
@@ -1,6 +1,5 @@
 package automation;
 
-
 //import org.junit.Test;
 import automation.driver.Driver;
 import org.junit.Assert;
@@ -18,57 +17,54 @@ import org.testng.annotations.Test;
 import java.time.Duration;
 import java.util.List;
 
-
 import static automation.base.Constants.*;
 import static automation.utils.RandomNameGenerator.*;
 
-
 public class SearchTestsTestng {
 
- //   private WebDriver driver;
-  WebDriver driver = Driver.getWebDriver();
+    WebDriver driver = Driver.getWebDriver();
 
-    @BeforeMethod (alwaysRun = true)
-    public void browserSetUp ()  {
-//        System.getProperty("webdriver.browser.name", "/Users/Anzhalika_Baradzina/Downloads/chromedriver");
-//        driver = new ChromeDriver();
+    @BeforeMethod(alwaysRun = true)
+    public void browserSetUp() {
+
         driver.manage().window().maximize();
         driver.get(BASE_URL);
     }
 
-    @Test (description = "Search results test")
-    public void commonSearchTermResultsNotEmpty()
-    {
+    @Test(description = "Search results test")
+    public void commonSearchTermResultsNotEmpty() {
         WebElement searchButton = driver.findElement(By.xpath(("//div[@class = 'header-search-ui header__control']")));
         searchButton.click();
         new WebDriverWait(driver, Duration.ofSeconds(5))
-                .until(ExpectedConditions.presenceOfElementLocated(By.xpath(("//div[@class = 'header-search__panel opened']"))));
+                .until(ExpectedConditions
+                        .presenceOfElementLocated(By.xpath(("//div[@class = 'header-search__panel opened']"))));
         WebElement inputField = driver.findElement(By.xpath(("//input[@id = 'new_form_search']")));
         inputField.sendKeys("Testing approach");
         WebElement findButton = driver.findElement(By.xpath(("//button[@class = 'header-search__submit']")));
         findButton.click();
         List<WebElement> searchResults = driver.findElements(By.xpath("//article[@class = 'search-results__item']"));
-        Assert.assertTrue("Search results are empty", searchResults.size()>0);
+        Assert.assertTrue("Search results are empty", searchResults.size() > 0);
     }
 
-    @Test (description = "Empty search results test")
-    public void searchResultsAreEmpty()
-    {
+    @Test(description = "Empty search results test")
+    public void searchResultsAreEmpty() {
         WebElement searchButton = driver.findElement(By.xpath(("//div[@class = 'header-search-ui header__control']")));
         searchButton.click();
-        new WebDriverWait(driver, Duration.ofSeconds(5) )
-                .until(ExpectedConditions.presenceOfElementLocated(By.xpath(("//div[@class = 'header-search__panel opened']"))));
+        new WebDriverWait(driver, Duration.ofSeconds(5))
+                .until(ExpectedConditions
+                        .presenceOfElementLocated(By.xpath(("//div[@class = 'header-search__panel opened']"))));
         WebElement inputField = driver.findElement(By.xpath(("//input[@id = 'new_form_search']")));
         inputField.sendKeys(generateRandomText(10));
         WebElement findButton = driver.findElement(By.xpath(("//button[@class = 'header-search__submit']")));
         findButton.click();
-        WebElement emptyResultsError = driver.findElement(By.xpath("//div[@class = 'search-results__exception-message search-results--empty-result']"));
+        WebElement emptyResultsError = driver.findElement(
+                By.xpath("//div[@class = 'search-results__exception-message search-results--empty-result']"));
         Assert.assertTrue("Search results are empty", emptyResultsError.isDisplayed());
 
     }
 
-    @AfterMethod (alwaysRun = true)
-    public void browserTearDown ()  {
+    @AfterMethod(alwaysRun = true)
+    public void browserTearDown() {
         Driver.close();
     }
 
