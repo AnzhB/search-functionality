c1193014fcf24ac6f4366f9a40efc6935b3ede92
Anzhalika Baradzina <abaradzina@corelogic.com>
2022-10-30 00:51:19 +0200

    RandomNameGenerator update

--- src/test/java/automation/utils/RandomNameGenerator.java
+++ src/test/java/automation/utils/RandomNameGenerator.java
@@ -6,26 +6,26 @@ public class RandomNameGenerator {
         return getAlphaNumericString(n);
     }
 
-   private static String getAlphaNumericString(int n) {
-        String AlphaNumericString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "abcdefghijklmnopqrstuvxyz";
+    private static String getAlphaNumericString(int n) {
+        String AlphacString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "abcdefghijklmnopqrstuvxyz";
         StringBuilder sb = new StringBuilder(n);
         for (int i = 0; i < n; i++) {
-            int index = (int)(AlphaNumericString.length() * Math.random());
-            sb.append(AlphaNumericString.charAt(index));
+            int index = (int) (AlphacString.length() * Math.random());
+            sb.append(AlphacString.charAt(index));
         }
         return sb.toString();
     }
 
     public static String getRandomNumber(int n) {
-      return getInteger(n);
+        return getInteger(n);
     }
 
-   private static String getInteger(int n) {
-        String AlphaNumericString = "0123456789";
+    private static String getInteger(int n) {
+        String NumericString = "0123456789";
         StringBuilder sb = new StringBuilder(n);
         for (int i = 0; i < n; i++) {
-            int index = (int)(AlphaNumericString.length() * Math.random());
-            sb.append(AlphaNumericString.charAt(index));
+            int index = (int) (NumericString.length() * Math.random());
+            sb.append(NumericString.charAt(index));
         }
         return sb.toString();
     }
