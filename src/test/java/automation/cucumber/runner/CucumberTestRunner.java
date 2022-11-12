6c7d35715fe4622ae119961e376ad27a5381f48f
Anzhalika Baradzina <abaradzina@corelogic.com>
2022-11-12 15:19:23 +0100

    CucumberTestRunner updated

--- src/test/java/automation/cucumber/runner/CucumberTestRunner.java
+++ src/test/java/automation/cucumber/runner/CucumberTestRunner.java
@@ -4,11 +4,20 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
+
+
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = "pretty",
+        plugin = {
+                "pretty",
+                "html:target/SystemTestReports/html",
+                "json:target/SystemTestReports/json/report.json",
+                "junit:target/SystemTestReports/junit/report.xml",
+        },
+
+        dryRun = false,
         monochrome = true,
-        tags = "@run",
+        tags = "@run and @smoke",
         glue = "automation",
         features ="src/test/java/resources/com/cucumber/Search.feature"
 
